69eed0e12d5ec736ada34ddea78eed7895e8f667
Yousef Alaqra <yousif.aqra@gmail.com>
2020-12-29 21:13:03 +0200

    setup api nodejs app

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+
+app.post("/hook", (req, res) => {
+    console.log(req.body) // Call your action on the request here
+    res.status(200).end() // Responding is important
+  })
